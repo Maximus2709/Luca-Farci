6342a5992c2b396b9675c726cd151c24634c86e1
Luca Farci <82846140+Maximus2709@users.noreply.github.com>
2024-02-19 15:15:55 +0000

    keep on going nearly finished

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,16 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Luca Farci",
+  title: "Web Designer & Game Developer",
+  email: "lucashousestudios@gmail.com",
+  gitHub: "/Maximus2709",
+  linkedIn: "/luca-farci-94370b8b/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#b738b5";
+const secondaryColor = "#18a05c";
 
 const App = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/keyboard.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "rgb keyboard showing aswd keys and a few other keys.";
 
 /**
  * Project list
